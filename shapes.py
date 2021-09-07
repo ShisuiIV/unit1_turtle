858eda0ba476e57eef61c3a7e4fed6b46aa0f491
ShisuiIV <fyalltr@ssfs.org>
2021-09-07 09:55:21 -0400

    working shapes

---
+++ shapes.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.forward (100)
+turtle.left (120)
+turtle.forward (100)
+turtle.left (120)
+turtle.forward (100)
+turtle.penup()
+turtle.forward(90)
+turtle.pendown()
+turtle.forward(100)
+turtle.left (90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.penup()
+turtle.forward(120)
+turtle.pendown()
+
+
+turtle.exitonclick()
+
+
