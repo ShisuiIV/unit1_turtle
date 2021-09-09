1819cc2247bf4f8e851953fae890aaae000691e8
ShisuiIV <fyalltr@ssfs.org>
2021-09-09 11:21:01 -0400

    working shapes

---
+++ rotated_shapes.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.speed(100)
+for x in range(90):
+
+    for x in range(4):
+        turtle.right(90)
+        turtle.forward(100)
+    turtle.right(4)
+
+
+
---
+++ squares.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.speed(10)
+for x in range (60):
+    for y in range (6):
+        turtle.left(300)
+        turtle.forward(90)
+    turtle.rt(60)
+
+
+
